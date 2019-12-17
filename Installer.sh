1769b406d27b383de2ee4f7273ee561bfc9323f1
Wine <43067887+WineSxc@users.noreply.github.com>
2019-12-17 11:00:50 -0500

    Add files via upload

---
+++ Installer.sh
@@ -0,0 +1,10 @@
+echo 'You are pretty dumb if you have to run this but whatever'
+sleep 2
+echo 'or lazy i guess?'
+apt-get update
+apt-get upgrade -y
+apt-get install python3
+apt-get install tshark
+pip install colorama 
+
+echo 'done you lazy prick'
